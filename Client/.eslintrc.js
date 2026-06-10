578cbe3039b5d2a9696a649090568f87ad7aefa5
harshithsai <harshithsai@gmail.com>
2026-06-10 18:54:31 +0530

    build time is reduced and no execution fails occur

--- Client/.eslintrc.js
+++ Client/.eslintrc.js
@@ -1,4 +1,28 @@
+// module.exports = {
+//   root: true,
+//   extends: '@react-native',
+// };
+
+
+
+
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: [
+    'babel.config.js',
+    'metro.config.js',
+    'node_modules/**',
+    'android/**',
+    'ios/**',
+    'packages/**',
+  ],
+  rules: {
+    // Disable rules that cause issues with RN 0.85
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'off',
+    'react/no-unstable-nested-components': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
+    '@typescript-eslint/no-redundant-type-constituents': 'off',
+  },
 };
--- Client/metro.config.js
+++ Client/metro.config.js
@@ -1,11 +1,11 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = {
+  resolver: {
+    blockList: [/\.cxx\/.*/],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
